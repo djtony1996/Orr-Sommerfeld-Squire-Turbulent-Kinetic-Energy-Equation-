ae6032b7738e27200b5f0e84b277965246f0003a
djtony1996 <77943130+djtony1996@users.noreply.github.com>
2024-08-06 16:52:28 +1000

    Update MAIN_OSS_TKE.py

--- MAIN_OSS_TKE.py
+++ MAIN_OSS_TKE.py
@@ -6,13 +6,15 @@ Created on Thu Aug  1 15:41:42 2024
 @author: jitongd
 """
 
+import sys
 import numpy as np
-import matplotlib.pyplot as plt
 from read_file import get_xyzuvw_channelflow
 from cheb_numeric import cheb
 from derivative_calculation import fft_xy, get_3d_physical, get_kx_array_ky_array, getkm
 from OSS_TKE import get_Orr_Sommerfeld_terms, get_Orr_Sommerfeld_D_terms, get_Squire_terms
 
+kx_index = int(sys.argv[1])
+ky_index = int(sys.argv[2])
 Retau = 180
 nx = 74
 ny = 74
@@ -43,49 +45,156 @@ d2Udz2 = channelRe['Up_diff2'][1:-1]
 kx_array, ky_array = get_kx_array_ky_array(nx_d,ny_d,dkx,dky)
 kx_m, ky_m         = getkm(kx_array, ky_array, nx, ny, dkx, dky)
 
-kx_index = 1
-ky_index = 1
 kx = kx_m[ky_index,kx_index]
 ky = ky_m[ky_index,kx_index]
 
 L  = D2 - (kx**2 + ky**2) * np.eye(nz-1)
 L2 = D4 + (kx**2 + ky**2)**2 * np.eye(nz-1) - 2 * (kx**2 + ky**2) * D2
 
+OS_first_avg   = np.zeros([nz-1])
+OS_second_avg  = np.zeros([nz-1])
+OS_third_avg   = np.zeros([nz-1])
+OS_fourth_avg  = np.zeros([nz-1])
+OS_fifth_avg   = np.zeros([nz-1])
+OS_sixth_avg   = np.zeros([nz-1])
+OSD_first_avg  = np.zeros([nz-1])
+OSD_second_avg = np.zeros([nz-1])
+OSD_third_avg  = np.zeros([nz-1])
+OSD_fourth_avg = np.zeros([nz-1])
+OSD_fifth_avg  = np.zeros([nz-1])
+OSD_sixth_avg  = np.zeros([nz-1])
+S_first_avg    = np.zeros([nz-1])
+S_second_avg   = np.zeros([nz-1])
+S_third_avg    = np.zeros([nz-1])
+S_fourth_avg   = np.zeros([nz-1])
+S_fifth_avg    = np.zeros([nz-1])
 
-filename = 'U100.nc'
+read_array = np.arange(100,1101,1)
 
-x,y,z,u,v,w = get_xyzuvw_channelflow(filename)
+for k_array in range(len(read_array)):
+    filename = f"data180/uTot/U{read_array[k_array]}.nc"
+    
+    x,y,z,u,v,w = get_xyzuvw_channelflow(filename)
+    
+    u = u[1:-1,:,:]
+    v = v[1:-1,:,:]
+    w = w[1:-1,:,:]
+    u = u - U[1:-1, np.newaxis, np.newaxis]
+    
+    u_F = fft_xy(u)
+    v_F = fft_xy(v)
+    w_F = fft_xy(w)
+    
+    dudx,dudy,dudz = get_3d_physical(u, D1, nx_d, ny_d, nx, ny, dkx, dky)
+    dvdx,dvdy,dvdz = get_3d_physical(v, D1, nx_d, ny_d, nx, ny, dkx, dky)
+    dwdx,dwdy,dwdz = get_3d_physical(w, D1, nx_d, ny_d, nx, ny, dkx, dky)
+    
+    fx = -u*dudx - v*dudy - w*dudz
+    fy = -u*dvdx - v*dvdy - w*dvdz
+    fz = -u*dwdx - v*dwdy - w*dwdz
+    
+    fx_F = fft_xy(fx)
+    fy_F = fft_xy(fy)
+    fz_F = fft_xy(fz)
+    
+    u_F  = u_F[:,ky_index,kx_index]
+    v_F  = v_F[:,ky_index,kx_index]
+    w_F  = w_F[:,ky_index,kx_index]
+    fx_F = fx_F[:,ky_index,kx_index]
+    fy_F = fy_F[:,ky_index,kx_index]
+    fz_F = fz_F[:,ky_index,kx_index]
+    
+    OS_first,  OS_second,  OS_third,  OS_fourth,  OS_fifth,  OS_sixth  = get_Orr_Sommerfeld_terms(w_F,fx_F,fy_F,fz_F,kx,ky,U,d2Udz2,L,L2,D1,Retau,nz)
+    OSD_first, OSD_second, OSD_third, OSD_fourth, OSD_fifth, OSD_sixth = get_Orr_Sommerfeld_D_terms(w_F,fx_F,fy_F,fz_F,kx,ky,U,d2Udz2,L,L2,D1,Retau,nz)
+    S_first,   S_second,   S_third,   S_fourth,   S_fifth              = get_Squire_terms(u_F,v_F,w_F,fx_F,fy_F,kx,ky,U,dUdz,L,D1,Retau,nz)
 
-u = u[1:-1,:,:]
-v = v[1:-1,:,:]
-w = w[1:-1,:,:]
-u = u - U[1:-1, np.newaxis, np.newaxis]
+    OS_first_avg  = OS_first_avg*k_array/(k_array+1)  + OS_first/(k_array+1)
+    OS_second_avg = OS_second_avg*k_array/(k_array+1) + OS_second/(k_array+1)
+    OS_third_avg  = OS_third_avg*k_array/(k_array+1)  + OS_third/(k_array+1)
+    OS_fourth_avg = OS_fourth_avg*k_array/(k_array+1) + OS_fourth/(k_array+1)
+    OS_fifth_avg  = OS_fifth_avg*k_array/(k_array+1)  + OS_fifth/(k_array+1)
+    OS_sixth_avg  = OS_sixth_avg*k_array/(k_array+1)  + OS_sixth/(k_array+1)
+    
+    OSD_first_avg  = OSD_first_avg*k_array/(k_array+1)  + OSD_first/(k_array+1)
+    OSD_second_avg = OSD_second_avg*k_array/(k_array+1) + OSD_second/(k_array+1)
+    OSD_third_avg  = OSD_third_avg*k_array/(k_array+1)  + OSD_third/(k_array+1)
+    OSD_fourth_avg = OSD_fourth_avg*k_array/(k_array+1) + OSD_fourth/(k_array+1)
+    OSD_fifth_avg  = OSD_fifth_avg*k_array/(k_array+1)  + OSD_fifth/(k_array+1)
+    OSD_sixth_avg  = OSD_sixth_avg*k_array/(k_array+1)  + OSD_sixth/(k_array+1)
 
-u_F = fft_xy(u)
-v_F = fft_xy(v)
-w_F = fft_xy(w)
+    S_first_avg  = S_first_avg*k_array/(k_array+1)  + S_first/(k_array+1)
+    S_second_avg = S_second_avg*k_array/(k_array+1) + S_second/(k_array+1)
+    S_third_avg  = S_third_avg*k_array/(k_array+1)  + S_third/(k_array+1)
+    S_fourth_avg = S_fourth_avg*k_array/(k_array+1) + S_fourth/(k_array+1)
+    S_fifth_avg  = S_fifth_avg*k_array/(k_array+1)  + S_fifth/(k_array+1)
 
-dudx,dudy,dudz = get_3d_physical(u, D1, nx_d, ny_d, nx, ny, dkx, dky)
-dvdx,dvdy,dvdz = get_3d_physical(v, D1, nx_d, ny_d, nx, ny, dkx, dky)
-dwdx,dwdy,dwdz = get_3d_physical(w, D1, nx_d, ny_d, nx, ny, dkx, dky)
 
-fx = -u*dudx - v*dudy - w*dudz
-fy = -u*dvdx - v*dvdy - w*dvdz
-fz = -u*dwdx - v*dwdy - w*dwdz
+if kx != 0 and ky != 0:
+    kx = -kx
+    kx_index = -kx_index
+    
+    for k_array in range(len(read_array)):
+        filename = f"data180/uTot/U{read_array[k_array]}.nc"
+        
+        x,y,z,u,v,w = get_xyzuvw_channelflow(filename)
+        
+        u = u[1:-1,:,:]
+        v = v[1:-1,:,:]
+        w = w[1:-1,:,:]
+        u = u - U[1:-1, np.newaxis, np.newaxis]
+        
+        u_F = fft_xy(u)
+        v_F = fft_xy(v)
+        w_F = fft_xy(w)
+        
+        dudx,dudy,dudz = get_3d_physical(u, D1, nx_d, ny_d, nx, ny, dkx, dky)
+        dvdx,dvdy,dvdz = get_3d_physical(v, D1, nx_d, ny_d, nx, ny, dkx, dky)
+        dwdx,dwdy,dwdz = get_3d_physical(w, D1, nx_d, ny_d, nx, ny, dkx, dky)
+        
+        fx = -u*dudx - v*dudy - w*dudz
+        fy = -u*dvdx - v*dvdy - w*dvdz
+        fz = -u*dwdx - v*dwdy - w*dwdz
+        
+        fx_F = fft_xy(fx)
+        fy_F = fft_xy(fy)
+        fz_F = fft_xy(fz)
+        
+        u_F  = u_F[:,ky_index,kx_index]
+        v_F  = v_F[:,ky_index,kx_index]
+        w_F  = w_F[:,ky_index,kx_index]
+        fx_F = fx_F[:,ky_index,kx_index]
+        fy_F = fy_F[:,ky_index,kx_index]
+        fz_F = fz_F[:,ky_index,kx_index]
+        
+        OS_first,  OS_second,  OS_third,  OS_fourth,  OS_fifth,  OS_sixth  = get_Orr_Sommerfeld_terms(w_F,fx_F,fy_F,fz_F,kx,ky,U,d2Udz2,L,L2,D1,Retau,nz)
+        OSD_first, OSD_second, OSD_third, OSD_fourth, OSD_fifth, OSD_sixth = get_Orr_Sommerfeld_D_terms(w_F,fx_F,fy_F,fz_F,kx,ky,U,d2Udz2,L,L2,D1,Retau,nz)
+        S_first,   S_second,   S_third,   S_fourth,   S_fifth              = get_Squire_terms(u_F,v_F,w_F,fx_F,fy_F,kx,ky,U,dUdz,L,D1,Retau,nz)
 
-fx_F = fft_xy(fx)
-fy_F = fft_xy(fy)
-fz_F = fft_xy(fz)
+        OS_first_avg  = OS_first_avg*(k_array+len(read_array))/(k_array+1+len(read_array))  + OS_first/(k_array+1+len(read_array))
+        OS_second_avg = OS_second_avg*(k_array+len(read_array))/(k_array+1+len(read_array)) + OS_second/(k_array+1+len(read_array))
+        OS_third_avg  = OS_third_avg*(k_array+len(read_array))/(k_array+1+len(read_array))  + OS_third/(k_array+1+len(read_array))
+        OS_fourth_avg = OS_fourth_avg*(k_array+len(read_array))/(k_array+1+len(read_array)) + OS_fourth/(k_array+1+len(read_array))
+        OS_fifth_avg  = OS_fifth_avg*(k_array+len(read_array))/(k_array+1+len(read_array))  + OS_fifth/(k_array+1+len(read_array))
+        OS_sixth_avg  = OS_sixth_avg*(k_array+len(read_array))/(k_array+1+len(read_array))  + OS_sixth/(k_array+1+len(read_array))
+        
+        OSD_first_avg  = OSD_first_avg*(k_array+len(read_array))/(k_array+1+len(read_array))  + OSD_first/(k_array+1+len(read_array))
+        OSD_second_avg = OSD_second_avg*(k_array+len(read_array))/(k_array+1+len(read_array)) + OSD_second/(k_array+1+len(read_array))
+        OSD_third_avg  = OSD_third_avg*(k_array+len(read_array))/(k_array+1+len(read_array))  + OSD_third/(k_array+1+len(read_array))
+        OSD_fourth_avg = OSD_fourth_avg*(k_array+len(read_array))/(k_array+1+len(read_array)) + OSD_fourth/(k_array+1+len(read_array))
+        OSD_fifth_avg  = OSD_fifth_avg*(k_array+len(read_array))/(k_array+1+len(read_array))  + OSD_fifth/(k_array+1+len(read_array))
+        OSD_sixth_avg  = OSD_sixth_avg*(k_array+len(read_array))/(k_array+1+len(read_array))  + OSD_sixth/(k_array+1+len(read_array))
 
-u_F  = u_F[:,ky_index,kx_index]
-v_F  = v_F[:,ky_index,kx_index]
-w_F  = w_F[:,ky_index,kx_index]
-fx_F = fx_F[:,ky_index,kx_index]
-fy_F = fy_F[:,ky_index,kx_index]
-fz_F = fz_F[:,ky_index,kx_index]
+        S_first_avg  = S_first_avg*(k_array+len(read_array))/(k_array+1+len(read_array))  + S_first/(k_array+1+len(read_array))
+        S_second_avg = S_second_avg*(k_array+len(read_array))/(k_array+1+len(read_array)) + S_second/(k_array+1+len(read_array))
+        S_third_avg  = S_third_avg*(k_array+len(read_array))/(k_array+1+len(read_array))  + S_third/(k_array+1+len(read_array))
+        S_fourth_avg = S_fourth_avg*(k_array+len(read_array))/(k_array+1+len(read_array)) + S_fourth/(k_array+1+len(read_array))
+        S_fifth_avg  = S_fifth_avg*(k_array+len(read_array))/(k_array+1+len(read_array))  + S_fifth/(k_array+1+len(read_array))
 
-OS_first_term,  OS_second_term,  OS_third_term,  OS_fourth_term,  OS_fifth_term,  OS_sixth_term  = get_Orr_Sommerfeld_terms(w_F,fx_F,fy_F,fz_F,kx,ky,U,d2Udz2,L,L2,D1,Retau,nz)
-OSD_first_term, OSD_second_term, OSD_third_term, OSD_fourth_term, OSD_fifth_term, OSD_sixth_term = get_Orr_Sommerfeld_D_terms(w_F,fx_F,fy_F,fz_F,kx,ky,U,d2Udz2,L,L2,D1,Retau,nz)
-S_first_term,   S_second_term,   S_third_term,   S_fourth_term,   S_fifth_term                   = get_Squire_terms(u_F,v_F,w_F,fx_F,fy_F,kx,ky,U,dUdz,L,D1,Retau,nz)
 
-plt.plot(z[1:-1],np.real(OSD_third_term))
+kx_int = int(kx)
+ky_int = int(ky)
+savename = f'OSS_TKE_onewavenumber{kx_int}_{ky_int}.npz'
+np.savez(savename,
+         OS_first_avg=OS_first_avg, OS_second_avg=OS_second_avg, OS_third_avg=OS_third_avg, OS_fourth_avg=OS_fourth_avg, OS_fifth_avg=OS_fifth_avg, OS_sixth_avg=OS_sixth_avg, 
+         OSD_first_avg=OSD_first_avg, OSD_second_avg=OSD_second_avg, OSD_third_avg=OSD_third_avg, OSD_fourth_avg=OSD_fourth_avg, OSD_fifth_avg=OSD_fifth_avg, OSD_sixth_avg=OSD_sixth_avg, 
+         S_first_avg=S_first_avg, S_second_avg=S_second_avg, S_third_avg=S_third_avg, S_fourth_avg=S_fourth_avg, S_fifth_avg=S_fifth_avg)
